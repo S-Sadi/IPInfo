784f5e3a3a9867e8ca45491b156379de50caa4a8
S-Sadi <shekhsadi100@gmail.com>
2022-08-11 01:32:22 +0600

    splash screen added

--- app/src/main/java/com/example/ipinfo/MainActivity.java
+++ app/src/main/java/com/example/ipinfo/MainActivity.java
@@ -26,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.Theme_MaterialComponents_NoActionBar_Bridge);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
